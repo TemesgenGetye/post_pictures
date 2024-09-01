a9ecac7455403072808dc7ca65e0f6f1e0567ea9
temesgen getye <temesgenselam1995@gmail.com>
2024-09-02 00:38:35 +0300

    server and ui link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,43 @@
 import Image from "next/image";
 import { db } from "~/server/db";
 
-const imageURl = [
-  "https://utfs.io/f/ca691885-9636-40a2-a083-27fbd1764917-1v7wuv.png",
-  "https://utfs.io/f/c215f3ac-afd2-4bd5-a220-ca90833048a5-d5hhcu.jpeg",
-  "https://utfs.io/f/5f8c8b04-a99c-4679-9423-605c0e0e1c66-26273.jpeg",
-  "https://utfs.io/f/c46fe6f9-c28a-4434-879f-d11ecb3c38e9-ob30i.png",
-  "https://utfs.io/f/f064c159-001d-482d-ace5-be30f38ab0fc-d5hhcu.png",
-  "https://utfs.io/f/ca691885-9636-40a2-a083-27fbd1764917-1v7wuv.png",
-  "https://utfs.io/f/c215f3ac-afd2-4bd5-a220-ca90833048a5-d5hhcu.jpeg",
-  "https://utfs.io/f/5f8c8b04-a99c-4679-9423-605c0e0e1c66-26273.jpeg",
-  "https://utfs.io/f/c46fe6f9-c28a-4434-879f-d11ecb3c38e9-ob30i.png",
-  "https://utfs.io/f/f064c159-001d-482d-ace5-be30f38ab0fc-d5hhcu.png",
-  "https://utfs.io/f/ca691885-9636-40a2-a083-27fbd1764917-1v7wuv.png",
-  "https://utfs.io/f/c215f3ac-afd2-4bd5-a220-ca90833048a5-d5hhcu.jpeg",
-  "https://utfs.io/f/5f8c8b04-a99c-4679-9423-605c0e0e1c66-26273.jpeg",
-  "https://utfs.io/f/c46fe6f9-c28a-4434-879f-d11ecb3c38e9-ob30i.png",
-  "https://utfs.io/f/f064c159-001d-482d-ace5-be30f38ab0fc-d5hhcu.png",
-];
-
-const images = imageURl.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export const daynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const post = await db.query.posts.findMany();
-  console.log(post);
+  const imageURL = await db.query.posts.findMany({
+    columns: {
+      id: true,
+      name: true,
+      url: true,
+    },
+    orderBy: (posts, { desc }) => [desc(posts.id)],
+  });
+  console.log(imageURL);
 
   return (
-    <main className="flex min-h-screen items-center p-4">
-      <div className="flex flex-wrap justify-center gap-4 py-1">
-        {post.map((post) => (
-          <div
-            key={post.id}
-            className="flex w-72 items-center justify-center gap-4 rounded-lg border border-[#ffd900d4] shadow-2xl"
-          >
-            <div>{post.name}</div>
-          </div>
-        ))}
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <div className="flex flex-wrap gap-4 py-1">
+        {[...imageURL, ...imageURL, ...imageURL]?.map((image) => (
+          <>
+            <div
+              key={image?.id}
+              className="m-auto flex flex-col items-center justify-center gap-1"
+            >
+              <Image
+                src={image?.url}
+                alt={`Image ${image?.id}`}
+                className="h-[300px] w-[300px] rounded-lg border border-[#ffd900d4] p-2 shadow-2xl"
+                width={300}
+                height={300}
+                content="cover"
+                priority={true}
+              />
 
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="flex items-center justify-center gap-4 rounded-lg border border-[#ffd900d4] shadow-2xl"
-          >
-            <Image
-              src={image.url}
-              alt={`Image ${image.id}`}
-              className="rounded-lg shadow-2xl"
-              width={300}
-              height={300}
-              content="cover"
-              priority={true}
-            />
-          </div>
+              <div className="text-start text-lg font-semibold">
+                {image?.name}
+              </div>
+            </div>
+          </>
         ))}
       </div>
     </main>
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,13 +2,6 @@ import * as schema from "./schema";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
-import {
-  pgTable,
-  serial,
-  text,
-  timestamp,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
 
 // Use this object to send drizzle queries to your DB
 export const db = drizzle(sql, { schema });
